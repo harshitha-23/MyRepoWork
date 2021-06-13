7989b6c12145ac18ee1c652312a63727eb5d7269
unknown <harshitha.yajamanam@gmail.com>
2021-06-13 18:52:16 +0530

    new file added

---
+++ logic.mango.py
@@ -0,0 +1,19 @@
+print("The program is for showing the list of items and each iteam cost")
+a=str(input("ITEAMS WE HAVE: PRESS ENTER "))
+print("1.mangoes,bananas,apples")
+b=str(input("what do u want from my list: "))
+if b== mangoes:
+    print("each mango cost is :10")
+elif b==bananas:
+    print("each bananas cost ois:20")
+else:
+    print("each apples cost is :5")
+
+#c=str(input("each friut cost is 10 : IF U WANT PRESS ENTER "))
+#d=int(input(" Enter How many fruits do u want : "))
+#k=d*10
+#print("YOUR FRUITS COST IS:{}  ".format(k))
+#print("PLZ TAKE YOUR ORDER SIR/MAM")
+#print("THANK YOU FOR COMING VISIT AGAIN")
+
+      
